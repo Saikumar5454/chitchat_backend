bc908cc6aa29f3c1f35d632108ed23e0dff4cdcd
Saikumar5454 <saikumar.789python@gmail.com>
2026-09-20 14:57:54 +0530

    added a frontend url to CORS so that backend allows requests

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def lifespan(_: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000",  "http://127.0.0.1:3001"],
+    allow_origins=["https://chitchat-frontend-alpha.vercel.app","http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000",  "http://127.0.0.1:3001"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
